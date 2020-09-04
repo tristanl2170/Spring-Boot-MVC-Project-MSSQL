306566fa431a4b650ed5aad2860e6e9cfd761533
Tristan <47876307+tristanl2170@users.noreply.github.com>
2020-09-03 21:52:33 -0400

    Add files via upload

---
+++ HealthcarePortalApplication.java
@@ -0,0 +1,13 @@
+package com.demo.healthcareportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcarePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcarePortalApplication.class, args);
+	}
+
+}
